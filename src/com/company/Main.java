2f9c8b82edf253a0c35c003cf5ca963166161bbd
Kerttu Lumi <kerttu.lumi@gmail.com>
2018-10-05 16:08:33 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
-package com.company;
+package com.kerttu;
 
-public class Main {
+public class Ring {
+    double raadius;
+    String varv;
 
-    public static void main(String[] args) {
-	// write your code here
+    public Ring() {
+        this.raadius = 0.0;
+        this.varv = "värvitu";
+    }
+
+    public Ring(double raadius, String varv) {
+        this.raadius = raadius;
+        this.varv = varv;
+    }
+
+    public double ringjoonePikkus() {
+        return 2 * Math.PI * this.raadius;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Ring{" +
+                "raasdius=" + raadius +
+                ", varv='" + varv + '\'' +
+                '}';
     }
 }
